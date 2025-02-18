7cff8d8e247ce41fafe7a508b6c4532933015385
Pedro Queiroz <pccql@cin.ufpe.br>
2025-02-18 20:58:31 -0300

    feat(tasks): memory expensive celery task

--- app/tasks/hello_world.py
+++ app/tasks/hello_world.py
@@ -1,6 +1,10 @@
 from celery_app import celery_app
 
+from tasks.memory_intensive_task import memory_intensive_task
+
 
 @celery_app.task()
 def hello_world():
+    memory_intensive_task.delay()
+
     return "Hello World"
