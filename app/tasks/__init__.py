e531eccc725a2c8db79e6057aec21fa2f3303eb9
Pedro Queiroz <pccql@cin.ufpe.br>
2025-02-18 21:06:25 -0300

    cpu and network intensive tasks

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,7 +1,6 @@
-from request_task import request_task
-
 from .cpu_intensive_task import cpu_intensive_task
 from .hello_world import hello_world
 from .memory_intensive_task import memory_intensive_task
+from .request_task import request_task
 
 __all__ = ["hello_world", "memory_intensive_task", "cpu_intensive_task", "request_task"]
